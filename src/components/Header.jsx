11d630ae5107337e1414f148de59d0c4da72ce52
hameed Shaik <hameedsk381@gmail.com>
2024-05-26 16:37:55 +0530

    landing page changed title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { Link as RouterLink } from 'react-router-dom';
-import { Container, Box } from '@mui/material';
+import { Container, Box, Avatar } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-
+import logo from '../assets/logotech.png'
 const Header = () => {
   const { t } = useTranslation();
 
@@ -14,9 +14,12 @@ const Header = () => {
     <AppBar position="static" color="primary">
       <Container>
         <Toolbar disableGutters>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {t('header.title')}
-          </Typography>
+          <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
+          <Avatar src={logo} alt="Logo" sx={{ height: 45, width: 45, marginRight: 2 }} />
+            <Typography variant="h6" component="div">
+              {t('header.title')}
+            </Typography>
+          </Box>
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
             <Button color="inherit" component={RouterLink} to="/">{t('header.home')}</Button>
             <Button color="inherit" href="#services">{t('header.services')}</Button>
