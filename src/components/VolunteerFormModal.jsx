e4c7da0e408b18ce8a2fe6d41b181a81dc6996c1
hameed Shaik <hameedsk381@gmail.com>
2024-06-02 12:59:47 +0530

    added volunteer registration post api

--- src/components/VolunteerFormModal.jsx
+++ src/components/VolunteerFormModal.jsx
@@ -44,7 +44,7 @@ const VolunteerFormModal = ({ open, handleClose }) => {
 
   const handleSubmit = async (values, { setSubmitting }) => {
     try {
-        await axiosInstance.post('api/volunteer', values);
+        await axios.post(`${serverUrl}/api/volunteer`, values);
       showSnackbar('Registration successful!', 'success');
       setSubmitting(false);
       handleClose();
